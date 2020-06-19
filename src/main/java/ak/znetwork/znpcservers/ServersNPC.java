74ba6d30b106dad580b7fb20059a4753e135627f
Savage34 <gustavog48121@gmail.com>
2020-06-19 13:41:03 -0300

    Initial commit

---
+++ src/main/java/ak/znetwork/znpcservers/ServersNPC.java
@@ -0,0 +1,11 @@
+package ak.znetwork.znpcservers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ServersNPC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
