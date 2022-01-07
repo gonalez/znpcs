c42630e02c60e493539e357842ba2c9d05275747
ZNetworkW <znetworkw.dev@gmail.com>
2022-01-07 14:07:48 -0300

    Add PluginContext concept

---
+++ src/main/java/io/github/znetworkw/znpcservers/context/PluginContext.java
@@ -0,0 +1,4 @@
+package io.github.znetworkw.znpcservers.context;/**
+ * @author Gaston Gonzalez {@literal <znetworkw.dev@gmail.com>}
+ */public interface PluginContext {
+}
---
+++ src/main/java/io/github/znetworkw/znpcservers/context/package-info.java
@@ -0,0 +1,3 @@
+/**
+ * @author Gaston Gonzalez {@literal <znetworkw.dev@gmail.com>}
+ */package io.github.znetworkw.znpcservers.context;
