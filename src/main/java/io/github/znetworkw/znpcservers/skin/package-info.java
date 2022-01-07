ea00f62598a3a7efb9142e8d057b75b5aba61300
ZNetworkW <znetworkw.dev@gmail.com>
2022-01-07 01:32:36 -0300

    Add package-info.java to skin package

---
+++ src/main/java/io/github/znetworkw/znpcservers/skin/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Skin fetchers are responsible for generating a skin result for a player name or other type.
+ *
+ * @author Gaston Gonzalez {@literal <znetworkw.dev@gmail.com>}
+ */
+package io.github.znetworkw.znpcservers.skin;
